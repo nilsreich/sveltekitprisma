56f10dc95082d816ceb0fedb3a1b91b58d048633
nilsreich <nilsreich@gmail.com>
2023-02-06 21:56:42 +0000

    jetzt aber

--- src/routes/api/updateBio/+server.ts
+++ src/routes/api/updateBio/+server.ts
@@ -1,7 +1,22 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
- 
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 export const POST = (async ({ request }) => {
   const { bio } = await request.json();
-  return json(bio);
+
+  const data = await prisma.profile.create({
+    data: {
+      bio: bio,
+      user: {
+        connect: {
+          id: 'cldspl7ey0000mg09yvpwsw7b'
+        }
+      }
+    }
+  })
+
+  return json(data);
 }) satisfies RequestHandler;
