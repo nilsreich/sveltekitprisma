f693c186d776ffe50d690b08279f1a1fdd440467
nilsreich <nilsreich@gmail.com>
2023-02-06 17:32:08 +0000

    test again

--- src/routes/api/updateBio/+server.ts
+++ src/routes/api/updateBio/+server.ts
@@ -2,8 +2,21 @@ import { PrismaClient } from '@prisma/client'
 import { json } from '@sveltejs/kit';
 const prisma = new PrismaClient()
 
-export const POST =  (async ({ request }) => {
-  const feed = await request.json()
-  const res = await fetch(feed.feed);
-  return json(weather);
+export const POST = (async ({ request }) => {
+  const body = await request.json()
+  console.log(body.bio, body.id)
+
+  const data = await prisma.profile.create({
+    data: {
+      bio: body.bio,
+      user: {
+        connect: {
+          id: body.id
+        }
+      }
+    },
+
+  })
+
+  return json({ data });
 });
--- src/routes/protected/+page.svelte
+++ src/routes/protected/+page.svelte
@@ -2,29 +2,21 @@
 	import { page } from '$app/stores';
 let url
 	const updateBio = async () => {
-		const feed= 'https://www.tagesschau.de/export/video-podcast/webm/tagesschau_https/'
+		const userID = 'cldspl7ey0000mg09yvpwsw7b'
+
 		const response = await fetch(`/api/updateBio`, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
 			},
-			body: JSON.stringify({ feed })
+						body: JSON.stringify({ bio:'my profile', id:userID })
+
 		});
 		const data = await response.json();
-		url = await data.match(/<enclosure url="(.*)" length/)[1];
-		console.log(url)
+		console.log(data)
 	};
 </script>
 
-{#if $page.data.session}
-	<h1>Protected page</h1>
-	<p>This is a protected content. You can access this content because you are signed in.</p>
-	<p>Session expiry: {$page.data.session?.expires}</p>
+
 
 	<button on:click={() => updateBio()}>update Bio</button>
-{:else}
-	<h1>Access Denied</h1>
-	<p>
-		<a href="/auth/signin"> You must be signed in to view this page </a>
-	</p>
-{/if}
