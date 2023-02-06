1dd0ba207c9ace50247c1973bc83291f809797fd
nilsreich <nilsreich@gmail.com>
2023-02-06 21:16:33 +0000

    POST

--- src/routes/api/updateBio/+server.ts
+++ src/routes/api/updateBio/+server.ts
@@ -3,7 +3,6 @@ import { json } from '@sveltejs/kit';
 const prisma = new PrismaClient()
 
 export const POST = (async () => {
-
   const data = await prisma.user.findMany();
 
   return json({ data });
