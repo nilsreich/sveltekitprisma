2ea195a0413cda6910328014497686ff8bc7fecb
nilsreich <nilsreich@gmail.com>
2023-02-06 21:09:16 +0000

    test

--- src/routes/api/updateBio/+server.ts
+++ src/routes/api/updateBio/+server.ts
@@ -2,21 +2,9 @@ import { PrismaClient } from '@prisma/client'
 import { json } from '@sveltejs/kit';
 const prisma = new PrismaClient()
 
-export const POST = (async ({ request }) => {
-  const body = await request.json()
-  console.log(body.bio, body.id)
+export const POST = (async () => {
 
-  const data = await prisma.profile.create({
-    data: {
-      bio: body.bio,
-      user: {
-        connect: {
-          id: body.id
-        }
-      }
-    },
-
-  })
+  const data = await prisma.user.findMany();
 
   return json({ data });
 });
--- src/routes/protected/+page.svelte
+++ src/routes/protected/+page.svelte
@@ -1,22 +1,11 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-let url
 	const updateBio = async () => {
-		const userID = 'cldspl7ey0000mg09yvpwsw7b'
-
 		const response = await fetch(`/api/updateBio`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-						body: JSON.stringify({ bio:'my profile', id:userID })
-
+			method: 'POST'
 		});
 		const data = await response.json();
-		console.log(data)
+		console.log(data);
 	};
 </script>
 
-
-
-	<button on:click={() => updateBio()}>update Bio</button>
+<button on:click={() => updateBio()}>update Bio</button>
