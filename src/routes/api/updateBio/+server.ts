feccc8742cc61199c027110b40dbfe7f8cafed61
nilsreich <nilsreich@gmail.com>
2023-02-06 22:02:30 +0000

    remove prisma

--- src/routes/api/updateBio/+server.ts
+++ src/routes/api/updateBio/+server.ts
@@ -3,18 +3,11 @@ import type { RequestHandler } from './$types';
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
-
+ 
 export const POST = (async ({ request }) => {
   const { bio } = await request.json();
 
-  await prisma.profile.create({
-    data: {
-      bio: bio,
-      user: {
-        connect: {
-          id: 'cldspl7ey0000mg09yvpwsw7b'
-        }
-      }
-    }
-  })
+
+
+  return json(bio);
 }) satisfies RequestHandler;
--- src/routes/protected/+page.svelte
+++ src/routes/protected/+page.svelte
@@ -1,15 +1,19 @@
 <script>
-	let bio = 'ich bin der test';
-
+	let bio ='ich bin der test'
+  
 	async function updateBio() {
-		await fetch('/api/updateBio', {
-			method: 'POST',
-			body: JSON.stringify({ bio }),
-			headers: {
-				'content-type': 'application/json'
-			}
-		});
+	  const response = await fetch('/api/updateBio', {
+		method: 'POST',
+		body: JSON.stringify({ bio }),
+		headers: {
+		  'content-type': 'application/json'
+		}
+	  });
+  
+	  const data = await response.json();
+	  console.log(data)
 	}
-</script>
+  </script>
 
-<button on:click={updateBio}>send bio</button>
+  
+  <button on:click={updateBio}>send bio</button>
