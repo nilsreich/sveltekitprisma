b481a588c083d75d8a242fbde3552e77e5e14a64
nilsreich <nilsreich@gmail.com>
2023-02-07 09:07:16 +0000

    on this way?

--- src/routes/api/updateBio/+server.ts
+++ src/routes/api/updateBio/+server.ts
@@ -18,7 +18,19 @@ const updatePost = async (id:number, bio:string)=>await prisma.post.update({
 export const POST = (async ({ request }) => {
   const { bio } = await request.json();
 
-  await updatePost(2, bio)
+  //  this doesnt work on vercel: 
+  //  await updatePost(2, bio)
+
+  // this works on vercel:
+  await prisma.post.update({
+    where: {
+      id: 2,
+    },
+    data: {
+      title: bio,
+    },
+  })
+  
 
   return json(bio);
 }) satisfies RequestHandler;
